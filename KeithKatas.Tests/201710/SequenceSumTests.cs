25927505a212c8fa66b3b0769632070a0cc81876
Keith Hodo <keith.hodo@premera.com>
2017-10-27 13:41:48 -0700

    Kata for October 27, 2017.

---
+++ KeithKatas.Tests/201710/SequenceSumTests.cs
@@ -0,0 +1,53 @@
+﻿using Kata.October2017;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KeithKatas.Tests._201710
+{
+    [TestClass]
+    public class SequenceSumTests
+    {
+        [TestMethod]
+        public void SequenceSum_Test_0()
+        {
+            int input = 0;
+            int[] expected = new int[] { 0 };
+
+            int[] actual = SequenceSum.SumOfN(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SequenceSum_Test_Ten()
+        {
+            int input = 10;
+            int[] expected = new int[] { 0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 };
+
+            int[] actual = SequenceSum.SumOfN(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SequenceSum_Test_NegativeFour()
+        {
+            int input = -4;
+            int[] expected = new int[] { 0, -1, -3, -6, -10 };
+
+            int[] actual = SequenceSum.SumOfN(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SequenceSum_Test_Neg7()
+        {
+            int input = -7;
+            int[] expected = new int[] { 0, -1, -3, -6, -10, -15, -21, -28 };
+
+            int[] actual = SequenceSum.SumOfN(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ KeithKatas/201710/SequenceSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kata.October2017
+{
+    public class SequenceSum
+    {
+        public static int[] SumOfN(int n)
+        {
+            var iterations = (int)Math.Sqrt(Math.Pow(n, 2));
+            var sumOfN = new int[iterations + 1];
+            var sum = 0;
+            var isPositive = n > -1;
+
+            for (int i = 0; i <= iterations; i++)
+            {
+                sum = isPositive ? sum + i : sum - i;
+                sumOfN[i] = sum;
+            }
+
+            return sumOfN;
+        }
+    }
+}
