54fa66348588a771f05999d90e32745235e45981
ssfcultra <koolkeith13@gmail.com>
2017-10-29 20:12:36 -0700

    Kata for October 29, 2017.

---
+++ KeithKatas.Tests/201710/SquareEveryDigitTests.cs
@@ -0,0 +1,47 @@
+﻿using Kata.October2017;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace KeithKatas.Tests.October2017
+{
+    [TestClass]
+    public class SquareEveryDigitTests
+    {
+        [TestMethod]
+        public void SquareEveryDigit_SquareDigits_SimpleTest1()
+        {
+            Assert.AreEqual(811181, SquareEveryDigit.SquareDigits(9119));
+        }
+
+        [TestMethod]
+        public void SquareEveryDigit_SquareDigits_SimpleTest2()
+        {
+            Assert.AreEqual(9414, SquareEveryDigit.SquareDigits(3212));
+        }
+
+        [TestMethod]
+        public void SquareEveryDigit_SquareDigits_SimpleTest3()
+        {
+            Assert.AreEqual(4114, SquareEveryDigit.SquareDigits(2112));
+        }
+
+        [TestMethod]
+        public void SquareEveryDigit_SquareDigits_Random()
+        {
+            var random = new Random();
+            int next;
+
+            for (int i = 0; i < 10; ++i)
+            {
+                next = random.Next(0, 1001);
+                Assert.AreEqual(Solution(next), SquareEveryDigit.SquareDigits(next));
+            }
+        }
+
+        private int Solution(int n)
+        {
+            return int.Parse(string.Join("", n.ToString().Select(d => int.Parse("" + d) * int.Parse("" + d))));
+        }
+    }
+}
---
+++ KeithKatas/201710/SquareEveryDigit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Kata.October2017
+{
+    public class SquareEveryDigit
+    {
+        public static int SquareDigits(int input)
+        {
+            var inputAsCharArray = input.ToString().ToCharArray();
+            var sb = new StringBuilder();
+
+            foreach (var character in inputAsCharArray)
+            {
+                var characterAsInt = int.Parse(character.ToString());
+                var valueForAppend = Math.Pow(characterAsInt, 2);
+                sb.Append(valueForAppend);
+            }
+
+            return int.Parse(sb.ToString());
+        }
+    }
+}
