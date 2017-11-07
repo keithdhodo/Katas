e25dc3e5c3bef09b4f86ea59900ab915b05696cc
ssfcultra <koolkeith13@gmail.com>
2017-11-07 14:54:21 -0800

    Kata for November 7, 2017

--- KeithKatas/201711/EasterSunday.cs
+++ KeithKatas/201711/EasterSunday.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KeithKatas.November2017
 {
@@ -23,50 +19,6 @@ namespace KeithKatas.November2017
             r = (2 * e + 2 * j - k - h + m + 32) % 7,
             month = (h - m + r + 90) / 25,
             day = (h - m + r + month + 19) % 32;
-
-            //String result;
-            //switch (n)
-            //{
-            //    case 1:
-            //        result = "January ";
-            //        break;
-            //    case 2:
-            //        result = "February ";
-            //        break;
-            //    case 3:
-            //        result = "March ";
-            //        break;
-            //    case 4:
-            //        result = "April ";
-            //        break;
-            //    case 5:
-            //        result = "May ";
-            //        break;
-            //    case 6:
-            //        result = "June ";
-            //        break;
-            //    case 7:
-            //        result = "July ";
-            //        break;
-            //    case 8:
-            //        result = "August ";
-            //        break;
-            //    case 9:
-            //        result = "September ";
-            //        break;
-            //    case 10:
-            //        result = "October ";
-            //        break;
-            //    case 11:
-            //        result = "November ";
-            //        break;
-            //    case 12:
-            //        result = "December ";
-            //        break;
-            //    default:
-            //        result = "error";
-            //        break;
-            //}
 
             return new DateTime(year, month, day);
         }
