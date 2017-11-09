1d8f26d285ef2fbe8171de7dac64f4777ea5fb5c
ssfcultra <koolkeith13@gmail.com>
2017-11-09 08:28:02 -0800

    Added Kata for November 9, 2017

---
+++ KeithKatas.Tests/201711/BasicMathTests.cs
@@ -0,0 +1,44 @@
+﻿using Kata.November2017;
+using NUnit.Framework;
+using System;
+
+namespace KeithKatas.Tests.November2017
+{
+    [TestFixture]
+    public class BasicPathTests
+    {
+        [Test]
+        public void BasicPathTests_StaticTests()
+        {
+            Assert.AreEqual(BasicMath.BasicOperation('+', 4, 7), 11);
+            Assert.AreEqual(BasicMath.BasicOperation('-', 15, 18), -3);
+            Assert.AreEqual(BasicMath.BasicOperation('*', 5, 5), 25);
+            Assert.AreEqual(BasicMath.BasicOperation('/', 49, 7), 7);
+        }
+
+        [Test]
+        public void BasicPathTests_RandomTests()
+        {
+            char[] ops = { '+', '-', '*', '/' };
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+
+                double val1 = rnd.Next(1, 999);
+                double val2 = rnd.Next(1, 999);
+
+                char op = ops[rnd.Next(1, 4)];
+
+                Assert.AreEqual(BasicMath.BasicOperation(op, val1, val2), getResult(op, val1, val2));
+            }
+        }
+        private static double getResult(char op, double val1, double val2)
+        {
+            if (op == '+') return val1 + val2;
+            if (op == '-') return val1 - val2;
+            if (op == '*') return val1 * val2;
+            if (op == '/') return val1 / val2;
+            return -1;
+        }
+    }
+}
---
+++ KeithKatas/201711/BasicMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Kata.November2017
+{
+    public class BasicMath
+    {
+        public static double BasicOperation(char operation, double value1, double value2)
+        {
+            if(!char.IsWhiteSpace(operation))
+            {
+                switch(operation)
+                {
+                    case '+': return value1 + value2;
+                    case '-': return value1 - value2;
+                    case '*': return value1 * value2;
+                    case '/': return value1 / value2;
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
