4cfd6a3887861542346e132bbc92f6dff5d9ff29
ssfcultra <koolkeith13@gmail.com>
2017-11-22 11:43:50 -0800

    Kata for November 22, 2017

---
+++ KeithKatas.Tests/201711/GaussTests.cs
@@ -0,0 +1,66 @@
+﻿using Kata.November2017;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace KeithKatas.Tests.November2017
+{
+    [TestFixture]
+    public class GaussTests
+    {
+        private static IEnumerable<TestCaseData> GaussTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(100l).Returns(5050l);
+                yield return new TestCaseData(300l).Returns(45150l);
+                yield return new TestCaseData(50000l).Returns(1250025000l);
+            }
+        }
+
+        [Test, TestCaseSource("GaussTestCases")]
+        public long? Gauss_Range_ValidTests(long n) => Gauss.RangeSum(n);
+
+        private static IEnumerable<TestCaseData> GaussErrorTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(0).Returns(null).SetDescription("returns null if n is 0");
+                yield return new TestCaseData(-10).Returns(null).SetDescription("returns null if n is less than 0");
+            }
+        }
+
+        [Test, TestCaseSource("GaussErrorTestCases")]
+        public long? Gauss_Range_ErrorTests(long n) => Gauss.RangeSum(n);
+
+        private static Random rnd = new Random();
+
+        private static long? GaussSolution(long n)
+        {
+            // Error handling
+            if (n < 1) { return null; }
+
+            // Triangular Number Formula:
+            // https://en.wikipedia.org/wiki/Triangular_number
+            return (n) * (n + 1) / 2;
+        }
+
+        [Test]
+        public void Gauss_Range_RandomTests()
+        {
+            const int Tests = 100;
+            const int MinValue = -2000000;
+            const int MaxValue = int.MaxValue / 6;
+
+            for (int i = 0; i < Tests; ++i)
+            {
+                long n = rnd.Next(MinValue, MaxValue + 1);
+
+                long? expected = GaussSolution(n);
+                long? actual = Gauss.RangeSum(n);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
---
+++ KeithKatas/201711/Gauss.cs
@@ -0,0 +1,32 @@
+﻿namespace Kata.November2017
+{
+    public class Gauss
+    {
+        public static long? RangeSum(long n)
+        {
+            // Note: This function's runtime can be made to be near-instant, but it should not be necessary for this Kata.
+
+            // Triangular Number Formula:
+            // https://en.wikipedia.org/wiki/Triangular_number
+            return n < 1 ? (long?) null : (n) * (n + 1) / 2;
+
+            // return n < 0 ? (long?)null : ExtensionLong.Range(0, n + 1).Sum();
+
+            //if (n < 0)
+            //{
+            //    return null;
+            //}
+            //else
+            //{
+            //    Int64 sum = 0;
+
+            //    for (Int64 i = 0; i <= n; i++)
+            //    {
+            //        sum += i;
+            //    }
+
+            //    return sum;
+            //}
+        }
+    }
+}
