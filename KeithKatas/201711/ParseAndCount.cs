1baea4ad79178b34dd8f33bb386db93ba5d0ebab
ssfcultra <koolkeith13@gmail.com>
2017-11-24 10:14:32 -0800

    Clever answer for November 24, 2017 kata

--- KeithKatas/201711/ParseAndCount.cs
+++ KeithKatas/201711/ParseAndCount.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Kata.November2017
 {
@@ -10,38 +7,43 @@ namespace Kata.November2017
     {
         public static int PaC(string str)
         {
-            var number = 0;
-            var numbers = str.Split(':')[1];
+            var num = Regex.Replace(str, ".+er: ", "");
+            return num.Contains('+') ? num.Split('+').Sum(int.Parse) :
+                   num.Contains('-') ? int.Parse(num.Split('-')[0]) - int.Parse(num.Split('-')[1]) :
+                                       int.Parse(num);
+            //    var number = 0;
+            //    var numbers = str.Split(':')[1];
 
-            if (numbers.Contains("+"))
-            {
-                var numbersAsArray = ConvertToInt(numbers, '+');
-                number = numbersAsArray[0] + numbersAsArray[1];
-            }
-            else if (numbers.Contains("-"))
-            {
-                var numbersAsArray = ConvertToInt(numbers, '-');
-                number = numbersAsArray[0] - numbersAsArray[1];
-            }
-            else
-            {
-                int.TryParse(numbers, out number);
-            }
+            //    if (numbers.Contains("+"))
+            //    {
+            //        var numbersAsArray = ConvertToInt(numbers, '+');
+            //        number = numbersAsArray[0] + numbersAsArray[1];
+            //    }
+            //    else if (numbers.Contains("-"))
+            //    {
+            //        var numbersAsArray = ConvertToInt(numbers, '-');
+            //        number = numbersAsArray[0] - numbersAsArray[1];
+            //    }
+            //    else
+            //    {
+            //        int.TryParse(numbers, out number);
+            //    }
 
-            return number;
-        }
+            //    return number;
+            //}
 
-        private static int[] ConvertToInt(string numbers, char operation)
-        {
-            var integerList = new List<int>();
+            //private static int[] ConvertToInt(string numbers, char operation)
+            //{
+            //    var integerList = new List<int>();
 
-            int leftNumber = 0;
-            int.TryParse(numbers.Split(operation)[0], out leftNumber);
-            integerList.Add(leftNumber);
-            int rightNumber = 0;
-            int.TryParse(numbers.Split(operation)[1], out rightNumber);
-            integerList.Add(rightNumber);
-            return integerList.ToArray();
+            //    int leftNumber = 0;
+            //    int.TryParse(numbers.Split(operation)[0], out leftNumber);
+            //    integerList.Add(leftNumber);
+            //    int rightNumber = 0;
+            //    int.TryParse(numbers.Split(operation)[1], out rightNumber);
+            //    integerList.Add(rightNumber);
+            //    return integerList.ToArray();
+            //}
         }
     }
 }
