190af11be349d7453b90f9ed33b120ff9b90084c
ssfcultra <koolkeith13@gmail.com>
2017-11-23 20:23:07 -0800

    Second Kata for November 23, 2017. Happy Thanksgiving!

---
+++ KeithKatas/201711/LargestFiveDigitSequence.cs
@@ -0,0 +1,29 @@
+﻿namespace Kata.November2017
+{
+    public class LargestFiveDigitSequence
+    {
+        public static int GetNumber(string str)
+        {
+            int largestValue = 0;
+
+            if (str.Length < 5)
+            {
+                int.TryParse(str, out largestValue);
+                return largestValue;
+            }
+
+            for (int i = 0; i < str.Length - 4; i++)
+            {
+                int valueToCompare = 0;
+                int.TryParse(str.Substring(i, 5), out valueToCompare);
+                
+                if (valueToCompare > largestValue)
+                {
+                    largestValue = valueToCompare;
+                }
+            }
+
+            return largestValue;
+        }
+    }
+}
