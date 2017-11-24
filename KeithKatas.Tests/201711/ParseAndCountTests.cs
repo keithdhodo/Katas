27502da5f4d87b2647fc426cdf4c7c7f8f0ae5c7
ssfcultra <koolkeith13@gmail.com>
2017-11-24 10:13:24 -0800

    Kata for November 24, 2017

---
+++ KeithKatas.Tests/201711/ParseAndCountTests.cs
@@ -0,0 +1,64 @@
+﻿using Kata.November2017;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithKatas.Tests.November2017
+{
+    [TestFixture]
+    public class ParseAndCountTests
+    {
+        [Test]
+        public void ParseAndCount_PaC_Checkex()
+        {
+            Assert.AreEqual(2281337, ParseAndCount.PaC("[wqettr1-2qwe+Enter number: 2281337"));
+
+            Assert.AreEqual(1561889, ParseAndCount.PaC("wqrrrq]q-++ Enter answer: 6334 +1555555"));
+
+            Assert.AreEqual(1549221, ParseAndCount.PaC("wqr+rq-+ Enter answer: 1555555 - 6334"));
+        }
+
+        public Dictionary<string, int> randomTests = new Dictionary<string, int>();
+
+        [Test]
+
+        public void ParseAndCount_PaC_RandomTests()
+        {
+            Dictionary<string, int> tests = new Dictionary<string, int>();
+            for (int i = 0; i < 1000; i++)
+            {
+                try
+                {
+                    int ans = new Random().Next(100, 1000000);
+                    tests.Add("Enter number: " + ans.ToString(), ans);
+                    int n1 = new Random().Next(0, 1000000);
+                    int n2 = new Random(DateTime.Now.Millisecond).Next(0, 1000000);
+                    if (new Random().Next(0, 1000) < 500)
+                        tests.Add("Enter answer: " + n1.ToString() + "+" + n2.ToString(), n1 + n2);
+                    else
+                        tests.Add("Enter answer: " + n1.ToString() + "-" + n2.ToString(), n1 - n2);
+                }
+                catch (Exception) { }
+            }
+
+            randomTests = tests;
+            Console.WriteLine("Random Tests loaded!");
+            ParseAndCount source = new ParseAndCount();
+            int testn = 0;
+            int output = 0;
+            foreach (var test in randomTests)
+            {
+                Console.WriteLine("Input: " + test.Key + " | Output: " + test.Value);
+                output = ParseAndCount.PaC(test.Key);
+                Console.WriteLine("Solution output: " + output.ToString());
+                Assert.AreEqual(test.Value, output);
+                Console.WriteLine("Test #" + testn.ToString() + " completed.");
+                testn++;
+            }
+            Console.WriteLine("Good Job!");
+        }
+    }
+}
---
+++ KeithKatas/201711/ParseAndCount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.November2017
+{
+    public class ParseAndCount
+    {
+        public static int PaC(string str)
+        {
+            var number = 0;
+            var numbers = str.Split(':')[1];
+
+            if (numbers.Contains("+"))
+            {
+                var numbersAsArray = ConvertToInt(numbers, '+');
+                number = numbersAsArray[0] + numbersAsArray[1];
+            }
+            else if (numbers.Contains("-"))
+            {
+                var numbersAsArray = ConvertToInt(numbers, '-');
+                number = numbersAsArray[0] - numbersAsArray[1];
+            }
+            else
+            {
+                int.TryParse(numbers, out number);
+            }
+
+            return number;
+        }
+
+        private static int[] ConvertToInt(string numbers, char operation)
+        {
+            var integerList = new List<int>();
+
+            int leftNumber = 0;
+            int.TryParse(numbers.Split(operation)[0], out leftNumber);
+            integerList.Add(leftNumber);
+            int rightNumber = 0;
+            int.TryParse(numbers.Split(operation)[1], out rightNumber);
+            integerList.Add(rightNumber);
+            return integerList.ToArray();
+        }
+    }
+}
