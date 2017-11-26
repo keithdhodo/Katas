26a9ea96aa8b2c71326f68e212b40067dafe98ef
ssfcultra <koolkeith13@gmail.com>
2017-11-26 14:04:05 -0800

    Kata for November 26, 2017

---
+++ KeithKatas.Tests/201711/FakeBinaryTests.cs
@@ -0,0 +1,37 @@
+﻿using Kata.November2017;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace KeithKatas.Tests.November2017
+{
+    [TestFixture]
+    public class FakeBinaryTests
+    {
+        [Test]
+        public void FakeBinary_ConvertToFakeBinary_BasicTests()
+        {
+            Assert.AreEqual("01011110001100111", FakeBinary.ConvertToFakeBinary("45385593107843568"));
+            Assert.AreEqual("101000111101101", FakeBinary.ConvertToFakeBinary("509321967506747"));
+            Assert.AreEqual("011011110000101010000011011", FakeBinary.ConvertToFakeBinary("366058562030849490134388085"));
+            Assert.AreEqual("01111100", FakeBinary.ConvertToFakeBinary("15889923"));
+            Assert.AreEqual("100111001111", FakeBinary.ConvertToFakeBinary("800857237867"));
+        }
+
+        [Test]
+        public void FakeBinary_ConvertToFakeBinary_RandomTests()
+        {
+            var rand = new Random();
+
+            for (var i = 0; i < 100; i++)
+            {
+                var len = rand.Next(1, 30);
+
+                var x = String.Concat(Enumerable.Range(0, len).Select(a => rand.Next(0, 10).ToString()).ToArray());
+
+                var expected = string.Concat(x.Select(a => a < '5' ? "0" : "1")); ;
+                Assert.AreEqual(expected, FakeBinary.ConvertToFakeBinary(x));
+            }
+        }
+    }
+}
---
+++ KeithKatas/201711/FakeBinary.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace Kata.November2017
+{
+    public class FakeBinary
+    {
+        public static string ConvertToFakeBinary(string v)
+        {
+            //var inputAsCharArray = v.ToCharArray();
+            //for(int i = 0; i < inputAsCharArray.Length; i++)
+            //{
+            //    if (int.Parse(inputAsCharArray[i].ToString()) < 5)
+            //    {
+            //        inputAsCharArray[i] = '0';
+            //    }
+            //    else
+            //    {
+            //        inputAsCharArray[i] = '1';
+            //    }
+            //}
+            //return new string(inputAsCharArray);
+
+            return string.Concat(v.Select(a => a < '5' ? "0" : "1"));
+        }
+    }
+}
