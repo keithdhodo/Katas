deb33ca75340dc9c39a20a5ec00336259c72f9c3
ssfcultra <koolkeith13@gmail.com>
2017-11-29 09:48:04 -0800

    Kata for November 29, 2017

---
+++ KeithKatas.Tests/201711/WeirdStringCasingTests.cs
@@ -0,0 +1,60 @@
+﻿using Kata.November2017;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace KeithKatas.Tests.November2017
+{
+    [TestFixture]
+    public class WeirdStringCasingTests
+    {
+        [Test]
+        public static void WeirdStringCasing_ToWeirdCase_ShouldWorkForSomeExamples()
+        {
+            Assert.AreEqual("ThIs", WeirdStringCasing.ToWeirdCase("This"));
+            Assert.AreEqual("Is", WeirdStringCasing.ToWeirdCase("is"));
+            Assert.AreEqual("ThIs Is A TeSt", WeirdStringCasing.ToWeirdCase("This is a test"));
+        }
+
+        [Test]
+        public static void WeirdStringCasing_ToWeirdCase_ShouldWorkForWhitespace()
+        {
+            Assert.AreEqual("", WeirdStringCasing.ToWeirdCase(""), "Empty strings should work too!");
+            Assert.AreEqual("    ", WeirdStringCasing.ToWeirdCase("    "), "Whitespace should work too!");
+        }
+        [Test]
+        public static void WeirdStringCasing_ToWeirdCase_ShouldWorkForSymbols()
+        {
+            Assert.AreEqual("!@#$%^&*()", WeirdStringCasing.ToWeirdCase("!@#$%^&*()"), "Odd characters have to work");
+        }
+        [Test]
+        public static void WeirdStringCasing_ToWeirdCase_ShouldWorkForMoreComplexStrings()
+        {
+            Assert.AreEqual("OnCe, WhEn I WaS YoUnG, I CoDeD On ThE AmIgA!", WeirdStringCasing.ToWeirdCase("Once, when I was young, I coded on the Amiga!"));
+        }
+
+        private static Random rnd = new Random();
+        private static string chars = "                 abcdefghijlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        private static string solution(string s) =>
+          String.Join(" ", s.Split(' ').Select(word => String.Concat(word.Select((ch, idx) => idx % 2 == 0 ? Char.ToUpper(ch) : Char.ToLower(ch)))));
+
+        [Test]
+        public void WeirdStringCasing_ToWeirdCase_RandomTest()
+        {
+            const int Tests = 1000;
+
+            for (int i = 0; i < Tests; ++i)
+            {
+                string s = new Regex(@"\s+").Replace(String.Concat(new char[rnd.Next(5, 100)].Select(_ => chars[rnd.Next(0, chars.Length)])), " ").Trim();
+                if (s == String.Empty) { s = "a"; }
+
+                string expected = solution(s);
+                string actual = WeirdStringCasing.ToWeirdCase(s);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
---
+++ KeithKatas/201711/WeirdStringCasing.cs
@@ -0,0 +1,34 @@
+﻿namespace Kata.November2017
+{
+    public class WeirdStringCasing
+    {
+        public static string ToWeirdCase(string s)
+        {
+            int counter = 0;
+            var charArray = s.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (counter % 2 == 0)
+                {
+                    charArray[i] = char.ToUpperInvariant(charArray[i]);
+                }
+                else
+                {
+                    charArray[i] = char.ToLowerInvariant(charArray[i]);
+                }
+
+                if (charArray[i] == ' ')
+                {
+                    counter = 0;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+
+            return new string(charArray);
+        }
+    }
+}
