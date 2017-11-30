e5474b52be6c25d24a0d7c61c7456c8b8adb2cac
ssfcultra <koolkeith13@gmail.com>
2017-11-30 10:32:39 -0800

    Kata for November 30, 2017

---
+++ KeithKatas.Tests/201711/ReplaceWithAlphabetPositionTests.cs
@@ -0,0 +1,54 @@
+﻿using Kata.November2017;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithKatas.Tests.November2017
+{
+    [TestFixture]
+    public class ReplaceWithAlphabetPositionTests
+    {
+        [Test]
+        public void ReplaceWithAlphabetPosition_AlphabetPosition_SampleTest()
+        {
+            Assert.AreEqual("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11", ReplaceWithAlphabetPosition.AlphabetPosition("The sunset sets at twelve o' clock."));
+            Assert.AreEqual("20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20", ReplaceWithAlphabetPosition.AlphabetPosition("The narwhal bacons at midnight."));
+        }
+
+        private static Random rnd = new Random();
+
+        public static string solution(string text)
+        {
+            List<string> result = new List<string>();
+
+            foreach (char c in text)
+            {
+                if (Char.IsLetter(c))
+                {
+                    result.Add(((int)Char.ToUpper(c) - 64).ToString());
+                }
+            }
+
+            return String.Join(" ", result);
+        }
+
+        [Test, Description("Random Tests")]
+        public void ReplaceWithAlphabetPosition_AlphabetPosition_RandomTest()
+        {
+            const int Tests = 1000;
+
+            for (int i = 0; i < Tests; ++i)
+            {
+                string text = String.Concat(new char[rnd.Next(20, 100)].Select(_ => (char)rnd.Next(128)));
+
+                string expected = solution(text);
+                string actual = ReplaceWithAlphabetPosition.AlphabetPosition(text);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
---
+++ KeithKatas/201711/ReplaceWithAlphabetPosition.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace Kata.November2017
+{
+    public class ReplaceWithAlphabetPosition
+    {
+        //private const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        //public static string AlphabetPosition(string text)
+        //{
+        //    text = text.ToUpperInvariant();
+
+        //    var sb = new StringBuilder();
+
+        //    for (int i = 0; i < text.Length; i++)
+        //    {
+        //        var indexOfChar = alphabet.IndexOf(text[i]);
+
+        //        if (indexOfChar > -1)
+        //        {
+        //            if ((sb.Length > 0) && (i > 0))
+        //            {
+        //                sb.Append(" ");
+        //            }
+
+        //            sb.Append(indexOfChar + 1);
+        //        }
+        //    }
+
+        //    return sb.ToString();
+        //}
+
+        public static string AlphabetPosition(string text)
+        {
+            return string.Join(" ", text.ToLower().Where(char.IsLetter).Select(x => x - 'a' + 1));
+        }
+    }
+}
