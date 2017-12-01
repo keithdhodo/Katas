0fb7581037259e1ef1ca56452900159171f9d007
ssfcultra <koolkeith13@gmail.com>
2017-12-01 14:13:42 -0800

    Kata for December 1, 2017

---
+++ KeithKatas.Tests/201712/ReturnNegativeTests.cs
@@ -0,0 +1,50 @@
+﻿using Kata.December2017;
+using NUnit.Framework;
+using System;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class ReturnNegativeTests
+    {
+        [Test]
+        public void ReturnNegative_MakeNegative_Test1()
+        {
+            Assert.AreEqual(-42, ReturnNegative.MakeNegative(42));
+        }
+
+        Random r = new Random();
+
+        [Test]
+        public void ReturnNegative_MakeNegative_PositiveTest()
+        {
+            Assert.AreEqual(-42, ReturnNegative.MakeNegative(42));
+        }
+
+        [Test]
+        public void ReturnNegative_MakeNegative_NegativeTest()
+        {
+            Assert.AreEqual(-9, ReturnNegative.MakeNegative(-9));
+        }
+
+        [Test]
+        public void ReturnNegative_MakeNegative_ZeroTest()
+        {
+            Assert.AreEqual(0, ReturnNegative.MakeNegative(0));
+        }
+
+        [Test]
+        public void ReturnNegative_MakeNegative_RandomPositiveTest()
+        {
+            int number = r.Next(1, 101);
+            Assert.AreEqual(-Math.Abs(number), ReturnNegative.MakeNegative(number));
+        }
+
+        [Test]
+        public void ReturnNegative_MakeNegative_RandomNegativeTest()
+        {
+            int number = r.Next(-100, 0);
+            Assert.AreEqual(-Math.Abs(number), ReturnNegative.MakeNegative(number));
+        }
+    }
+}
---
+++ KeithKatas/201712/ReturnNegative.cs
@@ -0,0 +1,10 @@
+﻿namespace Kata.December2017
+{
+    public class ReturnNegative
+    {
+        public static int MakeNegative(int number)
+        {
+            return (number < 0) ? number :  -1 * number;
+        }
+    }
+}
