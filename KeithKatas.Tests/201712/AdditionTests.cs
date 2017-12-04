a371d634b9544a3701a1b86942106e52cacba773
ssfcultra <koolkeith13@gmail.com>
2017-12-04 15:54:42 -0800

    Kata for December 4, 2017

---
+++ KeithKatas.Tests/201712/AdditionTests.cs
@@ -0,0 +1,15 @@
+﻿using Kata.December2017;
+using NUnit.Framework;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class AdditionTests
+    {
+        [Test]
+        public void Addition_Sum_Positives()
+        {
+            Assert.AreEqual(16, Addition.Sum(new[] { 6, 2, 1, 8, 10 }));
+        }
+    }
+}
---
+++ KeithKatas/201712/Addition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata.December2017
+{
+    public class Addition
+    {
+        public static int Sum(int[] numbers)
+        {
+            if(numbers == null || numbers.Length <= 1)
+            {
+                return 0;
+            }
+
+            Array.Sort(numbers);
+            return numbers.Skip(1).Take(numbers.Length - 2).Sum();
+        }
+    }
+}
