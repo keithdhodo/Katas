cbbf99cb9c1551f857ac1fd91bca049ae51dc71c
ssfcultra <koolkeith13@gmail.com>
2017-12-04 10:28:12 -0800

    Kata for December 3, 2017

---
+++ KeithKatas.Tests/201712/ClockTests.cs
@@ -0,0 +1,35 @@
+﻿using Kata.December2017;
+using NUnit.Framework;
+using System;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class ClockTests
+    {
+        [Test]
+        public void Clock_Past_Test()
+        {
+            Assert.AreEqual(61000, Clock.Past(0, 1, 1));
+        }
+
+        [Test]
+        public void Clock_Past_RandomTests()
+        {
+            Random r = new Random();
+
+            for (int d = 0; d < 100; d++)
+            {
+                int i = r.Next(50);
+                int j = r.Next(50);
+                int s = r.Next(50);
+                Assert.AreEqual(Past(i, j, s), Clock.Past(i, j, s), "Failed at Past(" + i + "," + j + "," + s + ")");
+            }
+        }
+
+        public int Past(int h, int m, int s)
+        {
+            return (h * 60 + m) * 60000 + s * 1000;
+        }
+    }
+}
---
+++ KeithKatas/201712/Clock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Kata.December2017
+{
+    public class Clock
+    {
+        private const int milli = 1000;
+
+        public static double Past(int h, int m, int s)
+        {
+            int seconds = s * milli;
+            int minutes = m * (milli * 60);
+            int hours = h * (milli * (int) Math.Pow(60, 2));
+
+            return hours + minutes + seconds;
+        }
+    }
+}
