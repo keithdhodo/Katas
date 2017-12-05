f93ec0f8a7ea2e65d79f463d6dc1efe35ddff84e
ssfcultra <koolkeith13@gmail.com>
2017-12-05 10:19:11 -0800

    Removed usings for December 5, 2017 kata

--- KeithKatas/201712/FullName.cs
+++ KeithKatas/201712/FullName.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Kata.December2017
+﻿namespace Kata.December2017
 {
     public class FullName
     {
