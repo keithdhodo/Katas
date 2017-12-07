e45007b91cea4c1fb722775c93e4ed5f13ddc457
ssfcultra <koolkeith13@gmail.com>
2017-12-07 10:19:22 -0800

    Kata for December 7, 2017

---
+++ KeithKatas.Tests/201712/InvertValueTests.cs
@@ -0,0 +1,39 @@
+﻿using Kata.December2017;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class InvertValueTests
+    {
+        [Test]
+        public void InvertValue_InvertValues_BasicTests()
+        {
+            Assert.AreEqual(new int[] { -1, -2, -3, -4, -5 }, InvertValue.InvertValues(new int[] { 1, 2, 3, 4, 5 }));
+            Assert.AreEqual(new int[] { -1, 2, -3, 4, -5 }, InvertValue.InvertValues(new int[] { 1, -2, 3, -4, 5 }));
+            Assert.AreEqual(new int[] { }, InvertValue.InvertValues(new int[] { }));
+            Assert.AreEqual(new int[] { 0 }, InvertValue.InvertValues(new int[] { 0 }));
+        }
+
+        [Test]
+        public void InvertValue_InvertValues_RandomTests()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                var results = Enumerable.Range(0, rand.Next(100))
+                        .Select(r => rand.Next(10))
+                        .ToArray();
+
+                Assert.AreEqual(Solve(results), InvertValue.InvertValues(results));
+            }
+        }
+
+        private int[] Solve(int[] input)
+        {
+            return input.Select(n => -n).ToArray();
+        }
+    }
+}
---
+++ KeithKatas/201712/InvertValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Kata.December2017
+{
+    public class InvertValue
+    {
+        public static int[] InvertValues(int[] input)
+        {
+            return input.Select(n => -n).ToArray();
+
+            //for (int i = 0; i < input.Length; i++)
+            //{
+            //    input[i] = input[i] * -1;
+            //}
+
+            //return input;
+        }
+    }
+}
