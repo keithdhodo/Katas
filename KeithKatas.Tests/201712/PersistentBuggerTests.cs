d89fcece321cabd56693c1dcf92266716beb2577
ssfcultra <koolkeith13@gmail.com>
2017-12-08 14:18:42 -0800

    Update to December 8, 2017 Kata

--- KeithKatas.Tests/201712/PersistentBuggerTests.cs
+++ KeithKatas.Tests/201712/PersistentBuggerTests.cs
@@ -1,10 +1,7 @@
 ﻿using Kata.December2017;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KeithKatas.Tests.December2017
 {
@@ -19,6 +16,30 @@ namespace KeithKatas.Tests.December2017
             Assert.AreEqual(0, PersistentBugger.Persistence(4));
             Assert.AreEqual(2, PersistentBugger.Persistence(25));
             Assert.AreEqual(4, PersistentBugger.Persistence(999));
+        }
+
+        private static int Sol(long n)
+        {
+            int count = 0;
+            while (n > 9)
+            {
+                count++;
+                n = n.ToString().Select(digit => int.Parse(digit.ToString())).Aggregate((x, y) => x * y);
+            }
+            return count;
+        }
+
+        [Test]
+        public static void PersistentBugger_Persistence_Random()
+        {
+            Console.WriteLine("\n 100 Random Tests ****************");
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int n = rnd.Next(10, 500000);
+                //Console.WriteLine("Numbers: n " + n);
+                Assert.AreEqual(Sol(n), PersistentBugger.Persistence(n));
+            }
         }
     }
 }
