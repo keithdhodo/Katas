30576bf1d5a8ed8dca5b038426c55ac49f59eafa
ssfcultra <koolkeith13@gmail.com>
2017-12-09 19:01:47 -0800

    Kata for December 9, 2017

---
+++ KeithKatas.Tests/201712/OppositeNumberTests.cs
@@ -0,0 +1,33 @@
+﻿using Kata.December2017;
+using NUnit.Framework;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class OppositeNumberTests
+    {
+        [Test]
+        public void OppositeNumber_Opposite_Test_1()
+        {
+            Assert.AreEqual(-1, OppositeNumber.Opposite(1));
+        }
+
+        [Test]
+        public void OppositeNumber_Opposite_Test_2()
+        {
+            Assert.AreEqual(0, OppositeNumber.Opposite(0));
+        }
+
+        [Test]
+        public void OppositeNumber_Opposite_Test_3()
+        {
+            Assert.AreEqual(-3000, OppositeNumber.Opposite(3000));
+        }
+
+        [Test]
+        public void OppositeNumber_Opposite_Test_4()
+        {
+            Assert.AreEqual(58000, OppositeNumber.Opposite(-58000));
+        }
+    }
+}
---
+++ KeithKatas/201712/OppositeNumber.cs
@@ -0,0 +1,10 @@
+﻿namespace Kata.December2017
+{
+    public class OppositeNumber
+    {
+        public static int Opposite(int number)
+        {
+            return number * -1;
+        }
+    }
+}
