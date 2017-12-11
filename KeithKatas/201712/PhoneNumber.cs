a499e3c77fb52b991347bd614b4bc3e1eafe81db
ssfcultra <koolkeith13@gmail.com>
2017-12-11 10:06:43 -0800

    Kata for December 11, 2017

---
+++ KeithKatas/201712/PhoneNumber.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.December2017
+{
+    public class PhoneNumber
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            var areaCode = IntegerArrayToString(numbers.Take(3));
+            var prefix = IntegerArrayToString(numbers.Skip(3).Take(3));
+            var suffix = IntegerArrayToString(numbers.Skip(6).Take(4));
+
+            return $"({areaCode}) {prefix}-{suffix}";
+        }
+
+		public static string IntegerArrayToString(IEnumerable<int> input)
+        {
+            return new string(input.Select(x => char.Parse(x.ToString())).ToArray());
+        }
+    }
+}
