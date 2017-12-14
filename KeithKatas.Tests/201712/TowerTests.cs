c1f292632e878bc1e99dc98772302c985a84a4fe
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2017-12-13 20:06:59 -0800

    Updates to Kata for December 13, 2017

--- KeithKatas.Tests/201712/TowerTests.cs
+++ KeithKatas.Tests/201712/TowerTests.cs
@@ -1,10 +1,7 @@
 ﻿using Kata.December2017;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KeithKatas.Tests.December2017
 {
