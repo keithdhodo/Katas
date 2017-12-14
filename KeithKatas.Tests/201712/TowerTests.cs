091c6a77285705f2e8bf6dfa89b60d7cd527a6e8
Author Name <koolkeith13@gmail.com>
2017-12-13 20:00:08 -0800

    Kata for December 13, 2017

---
+++ KeithKatas.Tests/201712/TowerTests.cs
@@ -0,0 +1,59 @@
+﻿using Kata.December2017;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class TowerTests
+    {
+        [Test]
+        public void Tower_TowerBuilder_BasicTests()
+        {
+            Assert.AreEqual(string.Join(",", new[] { "*" }), string.Join(",", Tower.TowerBuilder(1)));
+            Assert.AreEqual(string.Join(",", new[] { " * ", "***" }), string.Join(",", Tower.TowerBuilder(2)));
+            Assert.AreEqual(string.Join(",", new[] { "  *  ", " *** ", "*****" }), string.Join(",", Tower.TowerBuilder(3)));
+        }
+
+        [Test]
+        public void Tower_TowerBuilder_RandomTests()
+        {
+            var rand = new Random();
+
+            Func<int, string[]> myTowerBuilder = delegate (int nFloors)
+            {
+                string[] lines = new string[nFloors];
+                for (var i = 1; i <= nFloors; i++)
+                {
+                    lines[i - 1] = (new string(' ', nFloors - i) + new string('*', i * 2 - 1) + new string(' ', nFloors - i));
+                }
+                return lines;
+            };
+
+            var seq = Enumerable.Range(1, 100).ToArray();
+            for (int r = 0; r < 100; r++)
+            {
+                for (int p = 0; p < 100; p++)
+                {
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        var temp = seq[r];
+                        seq[r] = seq[p];
+                        seq[p] = temp;
+                    }
+                }
+            }
+
+            for (int r = 0; r < 100; r++)
+            {
+                var n = seq[r];
+                //Console.WriteLine(n);
+                Assert.AreEqual(string.Join(",", myTowerBuilder(n)), string.Join(",", Tower.TowerBuilder(n)));
+            }
+        }
+    }
+}
---
+++ KeithKatas/201712/Tower.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.December2017
+{
+    public class Tower
+    {
+        public static string[] TowerBuilder(int nFloors)
+        {
+            var tower = new string[nFloors];
+            int maxStars = 1;
+
+            for (int i = 2; i <= nFloors; i++)
+            {
+                maxStars += 2;
+            }
+
+            for(int i = tower.Length - 1; i >= 0; i--)
+            {
+                var whitespace = tower.Length - 1 - i;
+                tower[i] = new string(Enumerable.Repeat(' ', whitespace).ToArray()) +
+                    new string(Enumerable.Repeat('*', maxStars).ToArray()) + 
+                    new string(Enumerable.Repeat(' ', whitespace).ToArray());
+                maxStars -= 2;
+            }
+
+            return tower;
+        }
+    }
+}
