20e8e2b9bd9d510a42304881895e13fdf1a10ea0
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2017-12-18 10:26:26 -0800

    Kata for December 18, 2017

---
+++ KeithKatas.Tests/201712/CircleTests.cs
@@ -0,0 +1,45 @@
+﻿using Kata.December2017;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class CircleTests
+    {
+        private static double epsilon = 1e-6;
+
+        [Test]
+        public void Circle_GetArea_SampleTest()
+        {
+            Assert.That(Circle.GetArea(new Circle(new Point(10, 10), 30)), Is.EqualTo(2827.433388).Within(epsilon));
+            Assert.That(Circle.GetArea(new Circle(new Point(25, -70), 30)), Is.EqualTo(2827.433388).Within(epsilon));
+            Assert.That(Circle.GetArea(new Circle(new Point(-15, 5), 0)), Is.EqualTo(0).Within(epsilon));
+            Assert.That(Circle.GetArea(new Circle(new Point(-15, 5), 12.5)), Is.EqualTo(490.873852).Within(epsilon));
+        }
+
+        [Test]
+        public void Circle_GetArea_RandomTest()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; ++i)
+            {
+                Point point = new Point(rnd.NextDouble() * 100 - 50, rnd.NextDouble() * 100 - 50);
+                Double radius = rnd.NextDouble() * 50;
+                Circle circle = new Circle(point, radius);
+
+                Assert.That(Circle.GetArea(circle), Is.EqualTo(CircleArea(circle)).Within(epsilon));
+            }
+        }
+
+        public static double CircleArea(Circle circle)
+        {
+            return Math.PI * circle.Radius * circle.Radius;
+        }
+    }
+}
---
+++ KeithKatas/201712/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.December2017
+{
+    public class Circle
+    {
+        public Point Center { get; private set; }
+        public double Radius { get; private set; }
+
+        public Circle(Point center, double radius)
+        {
+            this.Center = center;
+            this.Radius = radius;
+        }
+
+        public static double GetArea(Circle circle)
+        {
+            return Math.PI * Math.Pow(circle.Radius, 2);
+        }
+    }
+}
