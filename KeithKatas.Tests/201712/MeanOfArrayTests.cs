9fc43af99ff1b5e44a306fb1e22976df4b5884a6
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2017-12-19 15:34:57 -0800

    Second Kata for December 19, 2017

---
+++ KeithKatas.Tests/201712/MeanOfArrayTests.cs
@@ -0,0 +1,38 @@
+﻿using Kata.December2017;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class MeanOfArrayTests
+    {
+        [Test]
+        [TestCase(new int[] { 2, 2, 2, 2 }, ExpectedResult = 2)]
+        [TestCase(new int[] { 5, 10 }, ExpectedResult = 7)]
+        public static int MeanOfArray_GetAverage_FixedTest(int[] marks)
+        {
+            return MeanOfArray.GetAverage(marks);
+        }
+
+        private static int Solution(int[] marks)
+        {
+            return (int)Math.Floor(new List<int>(marks).Average());
+        }
+
+        [Test]
+        public static void MeanOfArray_GetAverage_RandomTest([Random(1, 10, 98)]int x)
+        {
+            List<int> l = new List<int>();
+            Random r = new Random();
+            for (int i = 0; i < x; i++)
+            {
+                l.Add(r.Next(100) + 1);
+            }
+            int[] arr = l.ToArray();
+            Assert.AreEqual(Solution(arr), MeanOfArray.GetAverage(arr), string.Format("Should work for {0}", string.Join(", ", l.ToArray())));
+        }
+    }
+}
---
+++ KeithKatas/201712/MeanOfArray.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Kata.December2017
+{
+    public class MeanOfArray
+    {
+        public static int GetAverage(int[] marks)
+        {
+            return (int)marks.Average();
+        }
+    }
+}
