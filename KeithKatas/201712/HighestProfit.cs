143cf111fe1b3803147684377d4dc2363e1fd3ea
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2017-12-20 11:16:47 -0800

    Daily Kata for December 20, 2017

---
+++ KeithKatas/201712/HighestProfit.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Kata.December2017
+{
+    public class HighestProfit
+    {
+        public static int[] MinMax(int[] lst)
+        {
+            return new int[] { lst.Min(), lst.Max() };
+        }
+    }
+}
