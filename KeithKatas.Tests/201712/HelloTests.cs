3fc019e4ebb51a58d2be253b752237e6a1f844cf
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2017-12-25 19:49:29 -0800

    Kata for December 25, 2017. Merry Christmas to all!

---
+++ KeithKatas.Tests/201712/HelloTests.cs
@@ -0,0 +1,21 @@
+﻿using KeithKatas.December2017;
+using NUnit.Framework;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class HelloTests
+    {
+        [Test]
+        public void Hello_Greet_MyTest()
+        {
+            Assert.AreEqual(Hello.Greet("Niks"), "hello Niks!");
+            Assert.AreEqual(Hello.Greet(""), null);
+            Assert.AreEqual(Hello.Greet(null), null);
+            Assert.AreEqual(Hello.Greet("Frank Underwood"), "hello Frank Underwood!");
+            Assert.AreEqual(Hello.Greet(" "), "hello  !");
+            Assert.AreEqual(Hello.Greet("!!"), "hello !!!");
+            Assert.AreEqual(Hello.Greet("asdawidsaiodfwiodaosdp[awfodskawdsoafkwad@"), "hello asdawidsaiodfwiodaosdp[awfodskawdsoafkwad@!");
+        }
+    }
+}
---
+++ KeithKatas/201712/Hello.cs
@@ -0,0 +1,10 @@
+﻿namespace KeithKatas.December2017
+{
+    public class Hello
+    {
+        public static string Greet(string name)
+        {
+            return string.IsNullOrEmpty(name) ? null : $"hello {name}!";
+        }
+    }
+}
