337d49eb7c7dd779c8580ee36d58032dc6d93f1a
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2017-12-28 22:33:54 -0800

    Kata for December 28, 2017

---
+++ KeithKatas.Tests/201712/AgeTests.cs
@@ -0,0 +1,24 @@
+﻿using KeithKatas.December2017;
+using NUnit.Framework;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class AgeTests
+    {
+        [Test]
+        public void Age_GetAge_BasicTests()
+        {
+            Assert.AreEqual(0, Age.GetAge("0 years old"));
+            Assert.AreEqual(1, Age.GetAge("1 years old"));
+            Assert.AreEqual(2, Age.GetAge("2 years old"));
+            Assert.AreEqual(3, Age.GetAge("3 years old"));
+            Assert.AreEqual(4, Age.GetAge("4 years old"));
+            Assert.AreEqual(5, Age.GetAge("5 years old"));
+            Assert.AreEqual(6, Age.GetAge("6 years old"));
+            Assert.AreEqual(7, Age.GetAge("7 years old"));
+            Assert.AreEqual(8, Age.GetAge("8 years old"));
+            Assert.AreEqual(9, Age.GetAge("9 years old"));
+        }
+    }
+}
---
+++ KeithKatas/201712/Age.cs
@@ -0,0 +1,10 @@
+﻿namespace KeithKatas.December2017
+{
+    public class Age
+    {
+        public static int GetAge(string inputString)
+        {
+            return int.Parse(inputString.Split(' ')[0]);
+        }
+    }
+}
