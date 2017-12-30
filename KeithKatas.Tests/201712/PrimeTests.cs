61ae2e5180d807c3d174c4b9a945f8549f1bbf88
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2017-12-29 17:34:50 -0800

    Kata for December 29, 2017

---
+++ KeithKatas.Tests/201712/PrimeTests.cs
@@ -0,0 +1,62 @@
+﻿using KeithKatas.December2017;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class PrimeTests
+    {
+        [Test]
+        public void Prime_IsPrime_SampleTest()
+        {
+            Assert.IsFalse(Prime.IsPrime(0));
+            Assert.IsFalse(Prime.IsPrime(1));
+            Assert.IsTrue(Prime.IsPrime(2));
+            Assert.IsFalse(Prime.IsPrime(6));
+            Assert.IsFalse(Prime.IsPrime(8));
+            Assert.IsFalse(Prime.IsPrime(9));
+            Assert.IsFalse(Prime.IsPrime(-1));
+            Assert.IsTrue(Prime.IsPrime(5099));
+        }
+
+        [Test, Description("Random Tests")]
+        public void Prime_IsPrime_RandomTests()
+        {
+            var rnd = new Random();
+            const int Tests = 1000;
+
+            for (int i = 0; i < Tests; ++i)
+            {
+                int n = rnd.Next();
+
+                bool expected = IsPrime(n);
+                bool actual = Prime.IsPrime(n);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        private static Dictionary<int, bool> primesMemo = new Dictionary<int, bool>();
+
+        private static bool IsPrime(int n)
+        {
+            // Memo Check
+            if (primesMemo.ContainsKey(n)) { return primesMemo[n]; }
+
+            double sqrt = Math.Sqrt(n);
+            for (double i = 2; i <= sqrt; ++i)
+            {
+                if (n % i == 0)
+                {
+                    primesMemo.Add(n, false);
+                    return false;
+                }
+            }
+
+            primesMemo.Add(n, n >= 2);
+            return n >= 2;
+        }
+    }
+}
---
+++ KeithKatas/201712/Prime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KeithKatas.December2017
+{
+    public class Prime
+    {
+        public static bool IsPrime(int n)
+        {
+            if (n >= -2 && n < 2)
+            {
+                return false;
+            }
+            else
+            {
+                var iterations = n > 0 ? (int)Math.Sqrt(n) + 1 : (int)Math.Sqrt(n) - 1;
+
+                for (int i = 2; i < iterations; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
