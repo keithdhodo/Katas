4f36dfeb978fa542ee81b348e05ada32f7ad6e7d
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2017-12-30 18:35:36 -0800

    Kata for December 30, 2017

---
+++ KeithKatas.Tests/201712/UnfinishedLoopTests.cs
@@ -0,0 +1,41 @@
+﻿using KeithKatas.December2017;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class UnfinishedLoopTests
+    {
+        [Test]
+        public static void UnfinishedLoop_CreateList_FixedTest()
+        {
+            List<int> myValues = new List<int>(new int[] { 1 });
+            Assert.AreEqual(myValues, UnfinishedLoop.CreateList(1));
+
+            myValues = new List<int>(new int[] { 1, 2 });
+            Assert.AreEqual(myValues, UnfinishedLoop.CreateList(2));
+
+            myValues = new List<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+            Assert.AreEqual(myValues, UnfinishedLoop.CreateList(6));
+        }
+
+        [Test]
+        public static void UnfinishedLoop_CreateList_RandomTests()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int num = r.Next(100) + 1;
+                Assert.AreEqual(Solution(num), UnfinishedLoop.CreateList(num));
+            }
+        }
+
+        private static List<int> Solution(int num)
+        {
+            return Enumerable.Range(1, num).ToList();
+        }
+    }
+}
---
+++ KeithKatas/201712/UnfinishedLoop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KeithKatas.December2017
+{
+    public class UnfinishedLoop
+    {
+        public static List<int> CreateList(int number)
+        {
+            //List<int> list = new List<int>();
+
+            //for (int counter = 1; counter <= number; counter++)
+            //{
+            //    list.Add(counter);
+            //}
+
+            //return list;
+            return Enumerable.Range(1, number).ToList();
+        }
+    }
+}
