87b9cf7fbdc104dde0e941169ac4d8c9600dc244
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2017-12-31 14:32:39 -0800

    Kata for December 31, 2017 -- Happy New Year's Eve!

---
+++ KeithKatas.Tests/201712/OddNumbersTests.cs
@@ -0,0 +1,31 @@
+﻿using KeithKatas.December2017;
+using NUnit.Framework;
+using System;
+
+namespace KeithKatas.Tests.December2017
+{
+    [TestFixture]
+    public class OddNumbersTests
+    {
+        [Test]
+        public void OddNumbers_OddCount_Test()
+        {
+            Assert.AreEqual(7, OddNumbers.OddCount(15));
+            Assert.AreEqual(7511, OddNumbers.OddCount(15023));
+        }
+
+        [Test]
+        public void OddNumbers_OddCount_RandomTest()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; ++i)
+            {
+                // some very very poor ulong generator
+                ulong n = ((ulong)rnd.Next() << rnd.Next(0, 31)) + (ulong)rnd.Next();
+
+                Assert.AreEqual(n / 2, OddNumbers.OddCount(n));
+            }
+        }
+    }
+}
---
+++ KeithKatas/201712/OddNumbers.cs
@@ -0,0 +1,10 @@
+﻿namespace KeithKatas.December2017
+{
+    public class OddNumbers
+    {
+        public static ulong OddCount(ulong n)
+        {
+            return n / 2;
+        }
+    }
+}
