656aefb2d8d5d293b9bf660d9349e7a9d5e138b0
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2018-01-02 11:07:17 -0800

    Kata for January 2, 2018

---
+++ KeithKatas.Tests/201801/NumberTests.cs
@@ -0,0 +1,35 @@
+﻿using KeithKatas.January2018;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithKatas.Tests.January2018
+{
+    [TestFixture]
+    public class NumberTests
+    {
+        [Test]
+        public void Number_NumberToString_Test()
+        {
+            Assert.AreEqual("67", Number.ToString(67));
+            Assert.AreEqual("79585", Number.ToString(79585));
+            Assert.AreEqual("3", Number.ToString(1 + 2));
+            Assert.AreEqual("-1", Number.ToString(1 - 2));
+        }
+
+        private static Random rnd = new Random();
+
+        [Test]
+        public void Number_NumberToString_RandomTests()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                int num = rnd.Next(-1000, 1001);
+                Assert.AreEqual("" + num, Number.ToString(num));
+            }
+        }
+    }
+}
---
+++ KeithKatas/201801/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithKatas.January2018
+{
+    public class Number
+    {
+        public static object ToString(int num)
+        {
+            return num.ToString();
+        }
+    }
+}
