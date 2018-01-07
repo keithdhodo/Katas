2971fc25085a0d884283e25c73fc9069aa084668
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2018-01-07 13:13:34 -0800

    Kata for January 7, 2018

---
+++ KeithKatas.Tests/201801/SquareSumTests.cs
@@ -0,0 +1,45 @@
+﻿using KeithKatas.January2018;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace KeithKatas.Tests.January2018
+{
+    [TestFixture]
+    public class SquareSumTests
+    {
+        [Test, Description("Sample Test 1")]
+        public void SquareSum_SquareAndSum_SampleTest1()
+        {
+            Assert.AreEqual(9, SquareSum.SquareAndSum(new int[] { 1, 2, 2 }));
+        }
+
+        [Test, Description("Sample Test 2")]
+        public void SquareSum_SquareAndSum_SampleTest2()
+        {
+            Assert.AreEqual(5, SquareSum.SquareAndSum(new int[] { 1, 2 }));
+        }
+
+        [Test, Description("Sample Test 3")]
+        public void SquareSum_SquareAndSum_SampleTest3()
+        {
+            Assert.AreEqual(50, SquareSum.SquareAndSum(new int[] { 5, 3, 4 }));
+        }
+
+        private static Random rnd = new Random();
+
+        private static int Solution(int[] n) => n.Sum((v) => v * v);
+
+        [Test, Description("Random Tests")]
+        public void RandomTest()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                int[] test = new int[rnd.Next(1, 10)].Select(_ => rnd.Next(0, 1000)).ToArray();
+                int expected = Solution(test);
+                int actual = SquareSum.SquareAndSum(test);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
---
+++ KeithKatas/201801/SquareSum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace KeithKatas.January2018
+{
+    public class SquareSum
+    {
+        public static int SquareAndSum(int[] n)
+        {
+            return n.Select(x => (int)Math.Pow(x, 2)).Sum();
+        }
+    }
+}
