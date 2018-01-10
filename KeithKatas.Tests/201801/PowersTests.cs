8e01cec5c3e8e2fcbc9d7b9bf03c8906e623f7f4
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2018-01-10 10:49:36 -0800

    Kata for January 10, 2018

---
+++ KeithKatas.Tests/201801/PowersTests.cs
@@ -0,0 +1,38 @@
+﻿using KeithKatas.January2018;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Numerics;
+
+namespace KeithKatas.Tests.January2018
+{
+    [TestFixture]
+    public class PowersTests
+    {
+        [Test]
+        public void Powers_PowersOfTwo_BasicTests()
+        {
+            Assert.AreEqual(new BigInteger[] { 1 }, Powers.PowersOfTwo(0));
+            Assert.AreEqual(new BigInteger[] { 1, 2 }, Powers.PowersOfTwo(1));
+            Assert.AreEqual(new BigInteger[] { 1, 2, 4, 8, 16 }, Powers.PowersOfTwo(4));
+        }
+
+        [Test]
+        public void Powers_PowersOfTwo_RandomTests()
+        {
+            var rand = new Random();
+
+            Func<int, BigInteger[]> myPowersOfTwo = delegate (int n)
+            {
+                return Enumerable.Range(0, n + 1).Select(a => BigInteger.Pow(2, a)).ToArray();
+            };
+
+            var shuffle = Enumerable.Range(0, 201).OrderBy(a => rand.Next(-1, 2));
+
+            shuffle.ToList().ForEach(n =>
+            {
+                Assert.AreEqual(myPowersOfTwo(n), Powers.PowersOfTwo(n));
+            });
+        }
+    }
+}
---
+++ KeithKatas/201801/Powers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace KeithKatas.January2018
+{
+    public class Powers
+    {
+        public static BigInteger[] PowersOfTwo(int n)
+        {
+            return Enumerable.Range(1, n + 1).Select(x => new BigInteger(Math.Pow(2, x - 1))).ToArray();
+        }
+    }
+}
