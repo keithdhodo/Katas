824a248eb91256c4aedb89f15e68474c21cb5188
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2018-01-13 21:08:36 -0800

    Kata for January 13, 2018

---
+++ KeithKatas.Tests/201801/StringDuplicateTests.cs
@@ -0,0 +1,73 @@
+﻿using KeithKatas.January2018;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithKatas.Tests.January2018
+{
+    [TestFixture]
+    public class StringDuplicateTests
+    {
+        [Test]
+        public void StringDuplicate_DuplicateEncode_BasicTests()
+        {
+            Assert.AreEqual("(((", StringDuplicate.DuplicateEncode("din"));
+            Assert.AreEqual("()()()", StringDuplicate.DuplicateEncode("recede"));
+            Assert.AreEqual(")())())", StringDuplicate.DuplicateEncode("Success"), "should ignore case");
+            Assert.AreEqual("))((", StringDuplicate.DuplicateEncode("(( @"));
+        }
+
+        [Test]
+        public void StringDuplicate_DuplicateEncode_ParenthesesTests()
+        {
+            Assert.AreEqual("))((", StringDuplicate.DuplicateEncode("(( @"));
+            Assert.AreEqual(")))))(", StringDuplicate.DuplicateEncode(" ( ( )"));
+        }
+
+        [Test]
+        public void StringDuplicate_DuplicateEncode_RandomTests()
+        {
+            var rand = new Random();
+
+            Func<string, string> myDuplicateEncode = delegate (string word)
+            {
+                Dictionary<char, int> dict = new Dictionary<char, int>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (!dict.ContainsKey(char.ToLower(word[i])))
+                    {
+                        dict.Add(char.ToLower(word[i]), 0);
+                    }
+                    dict[char.ToLower(word[i])]++;
+                }
+                string result = "";
+
+                for (var i = 0; i < word.Length; i++)
+                {
+                    if (dict[char.ToLower(word[i])] == 1)
+                    {
+                        result += "(";
+                    }
+                    else
+                    {
+                        result += ")";
+                    }
+                }
+
+                return result;
+            };
+
+            for (int r = 0; r < 10; r++)
+            {
+                var length = rand.Next(10, 21);
+                var chars = "abcdeFGHIJklmnOPQRSTuvwxyz() @!";
+                var word = string.Concat(Enumerable.Range(0, length).Select(a => chars[rand.Next(0, chars.Length)]));
+
+                Assert.AreEqual(myDuplicateEncode(word), StringDuplicate.DuplicateEncode(word));
+            }
+        }
+    }
+}
---
+++ KeithKatas/201801/StringDuplicate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KeithKatas.January2018
+{
+    public class StringDuplicate
+    {
+        public static string DuplicateEncode(string word)
+        {
+            word = word.ToLowerInvariant();
+
+            var characters = word.GroupBy(c => c)
+                  .Select(g => new KeyValuePair<char, int>(g.Key, g.Count())).ToDictionary(x => x.Key, x => x.Value);
+
+            var encodedWord = new char[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (characters[word[i]] > 1)
+                {
+                    encodedWord[i] = ')';
+                }
+                else
+                {
+                    encodedWord[i] = '(';
+                }
+            }
+
+            return new string(encodedWord);
+        }
+    }
+}
