b30ae24fe3efeaf1a3486a3e1ab1fdce0ea8f780
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2018-01-14 12:15:32 -0800

    Kata for January 14, 2018

---
+++ KeithKatas/201801/Filter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KeithKatas.January2018
+{
+    public class Filter
+    {
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            return listOfItems.OfType<int>().Cast<int>();
+            //return listOfItems
+            //        .Where(item => item.GetType() != typeof(string))
+            //        .Select(str => {
+            //            int value;
+            //            bool success = int.TryParse(str.ToString(), out value);
+            //            return new { value, success };
+            //        })
+            //      .Where(pair => pair.success)
+            //      .Select(pair => pair.value);
+        }
+    }
+}
