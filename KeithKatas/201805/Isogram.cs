f52d978630968e4b826b2ee0eb1b92493710d86e
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2018-05-18 15:31:05 -0700

    Kata for 20180518. Getting back to it

---
+++ KeithKatas/201805/Isogram.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace KeithKatas.May2018
+{
+
+    public class Isogram
+    {
+        public static bool IsIsogram(string str)
+        {
+            return str.ToLowerInvariant().Distinct().Count() == str.Length;
+        }
+    }
+}
