7b2f4a8e1b36e2b078e219d37e4d83d4a17c9a8b
Keith Hodo (AVANADE INC) <v-kehod@microsoft.com>
2018-05-21 20:53:21 -0700

    Kata for 20180521

---
+++ KeithKatas.Tests/201805/DisemvowelTests.cs
@@ -0,0 +1,30 @@
+﻿using KeithKatas.May2018;
+using NUnit.Framework;
+
+namespace KeithKatas.Tests.May2018
+{
+    [TestFixture]
+    public class DisemvowelTests
+    {
+        [Test]
+        public void ShouldRemoveAllVowels()
+        {
+            Assert.AreEqual(Disemvowel.DisemvowelTrolls("This website is for losers LOL!"),
+            "Ths wbst s fr lsrs LL!");
+        }
+
+        [Test]
+        public void MultilineString()
+        {
+            Assert.AreEqual(Disemvowel.DisemvowelTrolls("No offense but,\nYour writing is among the worst I've ever read"),
+            "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd");
+        }
+
+        [Test]
+        public void OneMoreForGoodMeasure()
+        {
+            Assert.AreEqual(Disemvowel.DisemvowelTrolls("What are you, a communist?"),
+            "Wht r y,  cmmnst?");
+        }
+    }
+}
---
+++ KeithKatas/201805/Disemvowel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KeithKatas.May2018
+{
+    public static class Disemvowel
+    {
+        private static HashSet<char> vowels = new HashSet<char>("aeiou");
+
+        public static string DisemvowelTrolls(string str)
+        {
+            var disemvoweled = new string(str.Where(ch => !vowels.Contains(char.ToLowerInvariant(ch))).ToArray());
+            return disemvoweled;
+        }
+    }
+}
